994a31047051f6b34376507bf4f929f329ffb9cd
Fedor Soldatkin <fsoldatkin@yandex.ru>
2020-05-14 09:57:37 +0300

    Write test for trailing stop.

--- tests/integration/test_trailing_stop.py
+++ tests/integration/test_trailing_stop.py
@@ -1,4 +1,5 @@
 import unittest
+from time import sleep
 from unittest.mock import Mock
 
 from supervisor import Supervisor
@@ -13,6 +14,9 @@ class TrailingStopTests(unittest.TestCase):
         self.exchange_mock.get_position_size_ws.return_value = 0
         self.supervisor = Supervisor(interface=self.exchange_mock)
 
+        self.exchange_mock.conn.base_url = 'https://testnet.bitmex.com/api/v1'
+        self.exchange_mock.conn.get_tick_size.return_value = 0.5
+
     def tearDown(self) -> None:
         self.supervisor.exit_cycle()
 
@@ -20,9 +24,7 @@ class TrailingStopTests(unittest.TestCase):
         order = Order(order_type='Stop', side='Buy', qty=228, stop_px=10000)
         self.exchange_mock.get_open_orders_ws.return_value = [order]
 
-        self.exchange_mock.conn.base_url = 'https://testnet.bitmex.com/api/v1'
         self.exchange_mock.get_last_price_ws.return_value = 9000
-        self.exchange_mock.conn.get_tick_size.return_value = 0.5
 
         self.supervisor.add_trailing_order(order, offset=10)
         self.supervisor.run_cycle()
@@ -39,9 +41,7 @@ class TrailingStopTests(unittest.TestCase):
         order = Order(order_type='Stop', side='Sell', qty=228, stop_px=1000)
         self.exchange_mock.get_open_orders_ws.return_value = [order]
 
-        self.exchange_mock.conn.base_url = 'https://testnet.bitmex.com/api/v1'
         self.exchange_mock.get_last_price_ws.return_value = 1000
-        self.exchange_mock.conn.get_tick_size.return_value = 0.5
 
         self.supervisor.add_trailing_order(order, offset=10)
         self.supervisor.run_cycle()
@@ -53,3 +53,20 @@ class TrailingStopTests(unittest.TestCase):
         self.exchange_mock.get_last_price_ws.return_value = 8000
         order.tracker.max_price = 8000
         self.assertEqual(7200, order.stop_px)
+
+    def test_stop_tracking_after_fill_order(self):
+        order = Order(order_type='Stop', side='Sell', qty=228, stop_px=1000)
+        self.exchange_mock.get_open_orders_ws.return_value = [order]
+        self.exchange_mock.get_last_price_ws.return_value = 9000
+
+        self.supervisor.add_trailing_order(order, 10)
+
+        self.supervisor.run_cycle()
+
+        order.order_id = 1234
+        self.exchange_mock.get_open_orders_ws.return_value = []
+        self.exchange_mock.get_order_status_ws.return_value = 'Filled'
+
+        sleep(0.5)
+
+        self.assertTrue(order.tracker.exited)
