7372a4e55fa662f02f1c50346d0539cd2dda60ac
Fedor Soldatkin <forkcs@yandex.ru>
2022-03-12 17:44:01 +0300

    Fix typos in example.py

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ TEST_API_KEY = 'your-api-key'
 TEST_API_SECRET = 'your-api-secret'
 
 if __name__ == '__main__':
-    # Exchange is a interface-like class to call api methods
+    # Exchange is an interface-like class to call api methods
     exchange = Exchange(api_key=TEST_API_KEY, api_secret=TEST_API_SECRET,
                         test=True, symbol='XBTUSD')
     supervisor = Supervisor(interface=exchange)
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     trailing_stop = Order(order_type='Stop', stop_px=3000, qty=10, side='Sell')
 
     # attach some callbacks to stop-loss, note that events starts with "_"
-    # DO NOT USE stop_cycle() method into callbacks!!! It causes the deadlock
+    # DO NOT USE stop_cycle() method in callbacks!!! It causes deadlock
     stop_loss._on_reject = lambda: print('Rejected')
     stop_loss._on_fill = lambda: print('We lost position(')
 
@@ -33,7 +33,7 @@ if __name__ == '__main__':
     supervisor.add_order(stop_loss)
     supervisor.add_order(tp1)
     supervisor.add_order(tp2)
-    supervisor.add_trailing_order(trailing_stop, offset=10)  # order will trail on 10% distance from currenr price
+    supervisor.add_trailing_order(trailing_stop, offset=10)  # order will trail on 10% distance from current price
     input('Enter to enter position')
     supervisor.stop_cycle()
     supervisor.enter_by_market_order(10)
